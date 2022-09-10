75fab84d98c09320a6271db5e89bdc7ba9e90a50
Eltrac Koalar <hi@guhub.cn>
2022-09-10 18:03:32 +0800

    footer

--- includes/Footer.js
+++ includes/Footer.js
@@ -1,11 +1,15 @@
 import moment from 'moment';
-import { TbBrandNextjs, TbBrandReactNative, TbBrandVercel } from 'react-icons/tb'
+import { TbBrandNextjs, TbBrandReactNative, TbBrandVercel } from 'react-icons/tb';
+import { ExL } from '/components'
 
 export default function Footer(){
     return(
     <footer className="text-gray-400 text-sm text-center py-10 px-2">
-        <p><TbBrandReactNative/>/<TbBrandNextjs/> <TbBrandVercel/></p>
-        <p>Powered by NextJS, Hosted by Vercel, Built with <span className="text-red-600">♥</span> by Eltrac</p>
+        <p>bulit with&nbsp;
+          <a href="https://github.com/facebook/react" title="React" target="_blank"><TbBrandReactNative/></a>&nbsp;
+          <a href="https://nextjs.org/" title="Next.js" target="_blank"><TbBrandNextjs/></a>&nbsp;
+          <a href="https://vercel.com/" title="Vercel" target="_blank"><TbBrandVercel/></a></p>
+        <p>Made with <span className="text-red-600">♥</span> by Eltrac, inspired by <ExL link="https://spencerwoo.com/"><span className="text-sm">Spencer Woo</span></ExL></p>
         <p><a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a> &copy; {moment().format('YYYY')} Eltrac</p>
     </footer>
   )
